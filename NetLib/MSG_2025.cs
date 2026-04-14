027c794aa371df211e3cf9a658507c7bb0dd7bfc
Oliver <20210207@htlwrn.ac.at>
2026-04-14 08:35:26 +0200

    getset

--- NetLib/MSG_2025.cs
+++ NetLib/MSG_2025.cs
@@ -1,6 +1,6 @@
 public class MSG
 {
     public enum Type { SEARCH, RESULT, None };
-    public Type type = Type.None;
-    public string? message = "";
+    public Type type { get; set; } = Type.None;
+    public string? message { get; set; } = "";
 }
